c896a184e2c93e75d1030f8f781b445f54e43380
Avahe Kellenberger <avahe@protonmail.ch>
2023-04-12 14:31:50 -0400

    Simplified json parsing.

--- src/ufd.nim
+++ src/ufd.nim
@@ -1,4 +1,4 @@
-import std/[json, os, strutils, options, sequtils, uri]
+import std/[json, os, strformat, strutils, options, sequtils, uri, algorithm]
 import jester
 import characters, fuzzy
 
@@ -6,6 +6,7 @@ type
   Hitbox = object
     name: Option[string]
     url: string
+
   Move = object
     hitboxes: seq[Hitbox]
     move_name: string
@@ -23,6 +24,37 @@ type
     hops_actionable:Option[string]
     endlag: Option[string]
 
+  Stats = object
+    weight: string
+    gravity: string
+    walk_speed: string
+    run_speed: string
+    initial_dash: string
+    air_speed: string
+    total_air_acceleration: string
+    sh_fh_shff_fhff_frames: string
+    fall_speed_fast_fall_speed: string
+    oos_options: seq[string]
+    shield_grab: string
+    shield_drop: string
+    jump_squat: string
+
+  MiscData = object
+    stats: Stats
+    moves: seq[Move]
+    html_id: string
+
+  MoveSection = object
+    section_name: string
+    html_id: string
+    moves: seq[Move]
+
+  CharacterJson = object
+    ufd_url: string
+    name: string
+    move_sections: seq[MoveSection]
+    misc_data: MiscData
+
   Character = object
     name: string
     moves: Table[string, Move]
@@ -30,28 +62,22 @@ type
 proc newCharacter(name: string): Character =
   return Character(name: name, moves: initTable[string, Move]())
 
-var characterCache = initTable[string, Character]()
+var characterLookup = initTable[string, Character]()
 
 proc populateCache() =
   for file in walkFiles("./data/*.json"):
     let json = parseFile(file)
-    let name = json["name"].str.toLower().replace(" ", "_").replace(".", "")
-    var character = newCharacter(name)
-
-    for moveSection in json["move_sections"].elems:
-      for moveJson in moveSection["moves"]:
-        let moveName = moveJson["move_name"].str
-        character.moves[moveName] = moveJson.to(Move)
-    characterCache[name] = character
+    let characterName = splitFile(file).name
+    let characterJson = json.to(CharacterJson)
+    var character = newCharacter(characterName)
+    for moveSection in characterJson.move_sections:
+      for move in moveSection.moves:
+        character.moves[move.move_name] = move
+    characterLookup[characterName] = character
 
 echo "Populating cache..."
 populateCache()
 echo "Cache built successfully."
-
-proc findCharacter(characterName: string): Option[Character] =
-  let searchedNames = sortByScore(characterName.replace(" ", ""), characterList)
-  if searchedNames.len > 0 and characterCache.hasKey(searchedNames[0]):
-    return some(characterCache[searchedNames[0]])
 
 proc findMove(character: Character, moveName: string): Option[Move] =
   let movesList = character.moves.keys.toSeq()
@@ -64,12 +90,10 @@ routes:
     resp %characterList
 
   get "/characters/@character/@move":
-    let characterQuery = decodeUrl(@"character")
-    let characterOpt = findCharacter(characterQuery)
-    if characterOpt.isNone:
+    if not characterLookup.hasKey(@"character"):
       resp %"No character found with that name"
 
-    let character = characterOpt.get()
+    let character = characterLookup[@"character"]
     let moveQuery = decodeUrl(@"move")
     let moveOpt = findMove(character, moveQuery)
     if moveOpt.isNone:
